ca81cec27673b1799b194a4ed9aca03522e632e4
harry <harrylee1230@gmail.com>
2014-09-12 00:48:55 +0000

    style ask a tutor and conversations

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
+  
   # GET /questions
   # GET /questions.json
   def index
